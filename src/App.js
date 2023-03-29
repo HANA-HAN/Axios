be2930c5347087ecbd49c6f54ff9ee83c7a26379
hana <han02hana06@gmail.com>
2023-03-29 20:59:23 +0900

    예시

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react';
+import Axios from 'axios';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [user, setUser]=useState("");
+  useEffect(()=>{
+    //Axios.post("/api/users").then((response)=>{
+    Axios.get("/api/getuser").then((response)=>{
+      if(response.data){
+        setUser(response.data);
+      }else{
+        alert("failed to");
+      }
+    });
+  },[]);
+
+  return(
+    <div className='App'>
+      <div align='left' style={{margin:'20px'}}>
+        <h2>GCU React-Spring Integration</h2>
+        ID: {user.id}<br/>
+        Name: {user.name}<br/>
+        Date: {user.dob}<br/>
+      </div>
     </div>
   );
 }
